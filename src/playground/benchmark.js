debf6b981d68c572577a8f43c3ce13e50b5f01e6
Leondro Lio <github@leondrolio.com>
2021-04-12 06:25:40 +0100

    Temporarily publish dist

--- src/playground/benchmark.js
+++ src/playground/benchmark.js
@@ -54,7 +54,7 @@ const ScratchSVGRenderer = require('scratch-svg-renderer');
 const Scratch = window.Scratch = window.Scratch || {};
 
 const ASSET_SERVER = 'https://cdn.assets.scratch.mit.edu/';
-const PROJECT_SERVER = 'https://cdn.projects.scratch.mit.edu/';
+const PROJECT_SERVER = 'https://projects.scratch.mit.edu/';
 
 const SLOW = .1;
 
@@ -87,12 +87,7 @@ const loadProject = function () {
  * @returns {string} a URL to download a project file.
  */
 const getProjectUrl = function (asset) {
-    const assetIdParts = asset.assetId.split('.');
-    const assetUrlParts = [PROJECT_SERVER, 'internalapi/project/', assetIdParts[0], '/get/'];
-    if (assetIdParts[1]) {
-        assetUrlParts.push(assetIdParts[1]);
-    }
-    return assetUrlParts.join('');
+    return PROJECT_SERVER + asset.assetId;
 };
 
 /**
